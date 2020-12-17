7bb89103d98524f4763d615df8beb575623f4fd2
Chayan Datta <sonicxxx7@gmail.com>
2020-12-17 22:29:52 +0530

    Models added

--- college/models.py
+++ college/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class College(models.Model):
+    """Model for college."""
+
+    class OwnershipChoices(models.IntegerChoices):
+        """Choices for ownership."""
+        PRIVATE = 0, _('Private')
+        PUBLIC = 1, _('Public')
+        OTHER = 2, _('Other')
+
+    class InstitutionType(models.IntegerChoices):
+        """Choices for Institution type."""
+        GRADUATION_COLLEGE = 0,
+        DIPLOMA_COLLEGE = 1,
+        VOCATIONAL_TRAINING = 2
+
+    full_name = models.CharField(null=True, max_length=255)
+    abbreviated_name = models.CharField(null=True, max_length=50)
+    meta = models.TextField(null=True)
+    keywords = models.TextField(null=True)
+    city = models.CharField(null=True, max_length=50)
+    state = models.CharField(null=True, max_length=50)
+    brochure = models.FileField(null=True, upload_to='college/brochure/')
+    image = models.ImageField(null=True, upload_to='college/image/')
+    logo = models.ImageField(null=True, upload_to='college/logo/')
+    ownership = models.IntegerField(null=True, choices=OwnershipChoices.choices)
+    approval = models.CharField(null=True, max_length=100)
+    college_type = models.IntegerField(null=True, choices=InstitutionType.choices)
+    established_year = models.CharField(null=True, max_length=100)
+    slug = models.SlugField(null=True, max_length=50)
+    about = models.TextField(null=True)
+    is_top = models.BooleanField(null=True, default=False)
+    admission_process = models.TextField(null=True)
+    placements = models.TextField(null=True)
+    degrees = models.JSONField(null=True) #CSV field
+    stream_degree = models.JSONField(null=True) #CSV field
+    streams = models.JSONField(null=True) #CSV field
+    entrance_exams = models.JSONField(null=True) #CSV field
+    contacts = models.JSONField(null=True) #CSV field
+    images = models.JSONField(null=True) #CSV field
+
+    def __str__(self):
+        return '{}, {}'.format(self.abbreviated_name, self.city)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.full_name)
+        super(College, self).save(*args, **kwargs)
---
+++ profiles/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Profile(models.Model):
+    """Profile class based on top of user."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField()
+    display_pic = models.ImageField(upload_to='user/dp/')
+    mobile_number = models.CharField(max_length=20)
+    full_name = models.CharField(max_length=100)
+
+
+class OTPVerification(models.Model):
+    """OTP Verification stack trace."""
+
+    class VerifierTag(models.IntegerChoices):
+        """Reason for generating OTP."""
+        MAIL_VERIFICATION = 1, _('MAIL VERIFICATION')
+        PHONE_VERIFICATION = 2, _('PHONE VERIFICATION')
+        PASSWORD_RESET = 3, _('PASSWORD RESET')
+        OTHER = 0, _('Other')
+
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    verifier_tag = models.IntegerField(choices=VerifierTag.choices)
+    is_verified = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.profile.user.get_full_name()
