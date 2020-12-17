0b6e0a0ed6125654a337808f0b7f42d8d4dda35d
Chayan Datta <chayan.datta@decentro.tech>
2020-12-17 22:14:07 +0530

    Boilerplate

---
+++ myCollegeAI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for myCollegeAI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myCollegeAI.settings')
+
+application = get_asgi_application()
