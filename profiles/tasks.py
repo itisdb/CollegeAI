57581c1722744cbdb5201f9f8fd5fbcb82df6cec
Chayan Datta <sonicxxx7@gmail.com>
2020-12-26 20:53:12 +0530

    send_registration_mail_task() added

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 
+from profiles.controllers.authentication import RegistrationHandler
+
 
 @shared_task
-def send_registration_mail(profile_uuid):
-    return profile_uuid
+def send_registration_mail_task(profile_uuid):
+    RegistrationHandler().send_registration_mail(profile_uuid)
