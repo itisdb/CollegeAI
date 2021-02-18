4786f5d1718ce76572efb12ccebcae489ebfde71
Chayan Datta <sonicxxx7@gmail.com>
2021-02-18 13:48:32 +0530

    Handle settings change

--- settings/base.py
+++ settings/base.py
@@ -143,11 +143,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/v2/raw')
+    os.path.join(BASE_DIR, 'templates/v2/raw/assets/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Celery Configuration Options
