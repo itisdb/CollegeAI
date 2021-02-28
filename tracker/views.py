94e43fc6d3ae4f8668ce4043fb531d633b998819
Udit <bhardwaj.ud99@gmail.com>
2021-02-28 11:03:26 -0500

    second commit

--- tracker/views.py
+++ tracker/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from profiles.models import Profile
 from tracker.models import tracker
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def function(request, curr_page, page_from):
     if request.user.is_authenticated():
         profile = Profile.objects.get(user = request.user)
-        obj = tracker(profile = profile,ip_address = , curr_page=curr_page,page_from = page_from)
+        ip = get_client_ip(request)
+        obj = tracker(profile = profile,ip_address = ip, curr_page=curr_page,page_from = page_from)
         obj.save()
