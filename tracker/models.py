9c95b7f7ef85c5cca56f82037958cd146a411d3d
Udit <bhardwaj.ud99@gmail.com>
2021-02-28 03:20:05 -0500

    First Commit

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+class tracker(models.Model):
+    profile = models.ForeignKeyField(Profile)
+    ip_address = models.GenericIPAddressField()
+    curr_page = models.CharField(max_length=20, null=True, blank=True)
+    page_from  = models.CharField(max_length=20, null=True, blank=True)
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from profiles.models import Profile
+from tracker.models import tracker
 
-# Create your views here.
+def function(request, curr_page, page_from):
+    if request.user.is_authenticated():
+        profile = Profile.objects.get(user = request.user)
+        obj = tracker(profile = profile,ip_address = , curr_page=curr_page,page_from = page_from)
+        obj.save()
