7c0c3ad62fcb5dea6a1fe14cadb7fe97c9c3c9f2
Udit <bhardwaj.ud99@gmail.com>
2021-03-02 12:20:33 -0500

    made changes

--- tracker/models.py
+++ tracker/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from profiles.models import Profile
 
 class tracker(models.Model):
-    profile = models.ForeignKeyField(Profile)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     ip_address = models.GenericIPAddressField()
     curr_page = models.CharField(max_length=20, null=True, blank=True)
-    page_from  = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.ip_address
--- tracker/views.py
+++ tracker/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
-from profiles.models import Profile
 from tracker.models import tracker
+from tracker.utilities import get_client_ip
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-def function(request, curr_page, page_from):
+def log_to_tracker(request, curr_pages):
     if request.user.is_authenticated():
-        profile = Profile.objects.get(user = request.user)
+        profile = request.user.profile
         ip = get_client_ip(request)
-        obj = tracker(profile = profile,ip_address = ip, curr_page=curr_page,page_from = page_from)
+        obj = Tracker.objects.create(profile = profile,ip_address = ip, curr_page=curr_pages)
         obj.save()
