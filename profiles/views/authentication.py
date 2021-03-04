53323cc6fda59b19965c21663ef83db4fb3d564d
prihit <53579437+prihit@users.noreply.github.com>
2021-03-04 20:12:14 +0530

    ff (#21)
    * ff
    
    * social_auth
    
    * Auth work done
    
    * Dashboard updated
    
    * Dashboard url updated

--- profiles/views/authentication.py
+++ profiles/views/authentication.py
@@ -15,37 +15,38 @@ class LoginView(View):
         if username and password:
             user = authenticate(username=username, password=password)
             if not user:
-                return render(request, 'pages/auth/login.html', {'error': 'Username/Password is incorrect.'})
+                return render(request, 'v2/pages/public/home.html', {'error': 'Username/Password is incorrect.'})
             login(request, user)
             return redirect('profile:dashboard')
-        return render(request, 'pages/auth/login.html', {'error': 'Username and Password is mandatory.'})
+        return render(request, 'v2/pages/public/home.html', {'error': 'Username and Password is mandatory.'})
 
     def get(self, request):
-        return render(request, 'pages/auth/login.html')
+        return render(request, 'v2/pages/public/home.html')
 
 
 class RegisterView(View):
 
     def post(self, request):
         try:
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
+            name = request.POST['name']
+            name = name.split(" ",1)
+            first_name = name[0]
+            last_name = name[-1]
             username = request.POST['username']
             password = request.POST['password']
-            email = request.POST['email']
         except (AttributeError, KeyError):
-            return render(request, 'pages/auth/register.html', {'error': 'All fields are mandatory.'})
+            return render(request, 'v2/pages/public/home.html', {'error': 'All fields are mandatory.'})
         try:
-            user_obj = User.objects.create_user(username, email, password)
+            user_obj = User.objects.create_user(username, username, password)
         except BaseException:
-            return render(request, 'pages/auth/login.html', {'error': 'Username already exists.'})
+            return render(request, 'v2/pages/public/home.html', {'error': 'Username already exists.'})
         user_obj.first_name = first_name
         user_obj.last_name = last_name
         user_obj.save()
         Profile.objects.create(user=user_obj)
         user = authenticate(username=username, password=password)
         if not user:
-            return render(request, 'pages/auth/login.html', {'error': 'Username/Password is incorrect.'})
+            return render(request, 'v2/pages/public/home.html', {'error': 'Username/Password is incorrect.'})
         login(request, user)
         context = {
             'template_name' : 'welcome_mail.html',
@@ -60,14 +61,14 @@ class RegisterView(View):
         return redirect('profile:dashboard')
 
     def get(self, request):
-        return render(request, 'pages/auth/register.html')
+        return render(request,'v2/pages/public/home.html')
 
 
 class LogoutView(View):
 
     def get(self, request, *args, **kwargs):
         logout(request)
-        return render(request, 'pages/public/home.html', {'message': 'You have been logged out !'})
+        return render(request, 'v2/pages/public/home.html', {'message': 'You have been logged out !'})
 
 
 class ResetPasswordView(View):
