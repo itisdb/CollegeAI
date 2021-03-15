cf91bbb7b0e72867adbe2bf4d019fd0aaba413d2
prihit <53579437+prihit@users.noreply.github.com>
2021-03-16 02:13:45 +0530

    json data added to individual college page

--- settings/local.py
+++ settings/local.py
@@ -2,7 +2,11 @@ from settings.base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'mycollegeai',
+        'PASSWORD': 'mycollegeai',
+        'HOST': 'mycollegeai-test.clssqwrmm6ld.us-east-2.rds.amazonaws.com',
+        'PORT': 5432,
     }
 }
