45132dcb5cc9713d3d35e4c913a1ccf8a7b2fcb9
Chayan Datta <sonicxxx7@gmail.com>
2021-05-04 16:53:20 +0530

    adds college fields

--- college/models.py
+++ college/models.py
@@ -14,12 +14,6 @@ class College(BaseModel):
         PUBLIC = 1, _('Public')
         OTHER = 2, _('Other')
 
-    class InstitutionType(models.IntegerChoices):
-        """Choices for Institution type."""
-        GRADUATION_COLLEGE = 0,
-        DIPLOMA_COLLEGE = 1,
-        VOCATIONAL_TRAINING = 2
-
     class AssociatedMetrics(models.TextChoices):
         """Choices of metrics of each college."""
         OTHER = 'other'
@@ -30,7 +24,9 @@ class College(BaseModel):
     full_name = models.CharField(null=True, max_length=255)
     abbreviated_name = models.CharField(null=True, max_length=50)
     meta_title = models.CharField(null=True, blank=True, max_length=500)
-    meta = models.CharField(null=True, blank=True, max_length=500)
+    meta = models.TextField(null=True, blank=True)
+    meta_keywords = models.JSONField(null=True, blank=True, default=[])
+    about_us = models.TextField(null=True, blank=True)
     keywords = models.CharField(null=True, blank=True, max_length=500)
     established_year = models.CharField(max_length=4, null=True, blank=True)
     city = models.CharField(null=True, max_length=50)
@@ -39,7 +35,6 @@ class College(BaseModel):
     logo = models.ImageField(null=True, upload_to='college/logo/')
     ownership = models.IntegerField(null=True, choices=OwnershipChoices.choices)
     approval = models.CharField(null=True, max_length=100)
-    college_type = models.IntegerField(null=True, choices=InstitutionType.choices)
     slug = models.SlugField(null=True, blank=True, max_length=50)
     is_top = models.BooleanField(null=True, default=False)
     stream_degree = models.JSONField(null=True)
