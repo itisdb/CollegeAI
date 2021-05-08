5c3e4b7f526c78fbb0d07426539502716b257e6c
Chayan Datta <sonicxxx7@gmail.com>
2021-05-08 21:02:10 +0530

    added seo stuffs

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from college.models import College
 
 from profiles.models import Psychometry, Profile
 
+
 def custom_not_found_error(request, *args, **argv):
     response = render(request, 'pages/error/404.html')
     response.status_code = 404
@@ -19,30 +20,35 @@ def custom_internal_error(request, *args, **argv):
 
 
 def home(request):
-    colleges = College.objects.filter(
-        is_top=True,
-    )[:9]
+    colleges = College.objects.filter(is_top=True)[:9]
     return render(request, 'v2/pages/public/home.html', {
         'colleges': colleges
     })
 
+
 def refer(request):
     return render(request, 'v2/pages/public/refer.html')
+
 
 def about(request):
     return render(request, 'v2/pages/public/about.html')
 
+
 def contact(request):
     return render(request, 'v2/pages/public/contact.html')
+
 
 def career(request):
     return render(request, 'v2/pages/public/career.html')
 
+
 def advertising(request):
     return render(request, 'v2/pages/public/advertising.html')
 
+
 def terms(request):
     return render(request, 'v2/pages/public/terms.html')
+
 
 def privacy(request):
     return render(request, 'v2/pages/public/privacy.html')
--- myCollegeAI/urls.py
+++ myCollegeAI/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from base import views as base_views
 from base.views import PsychoView, CompareView
 
@@ -23,27 +25,29 @@ handler403 = 'base.views.custom_not_found_error'
 handler400 = 'base.views.custom_not_found_error'
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('college/', include(('college.urls', 'college'), namespace='college')),
-  path('profile/', include(('profiles.urls', 'profile'), namespace='profile')),
-  path('login/', LoginView.as_view(), name='login'),
-  path('accounts/', include('allauth.urls')),
-  path('social-auth/', include('social_django.urls', namespace="social")),
-  path('logout/', LogoutView.as_view(), name='logout'),
-#   path('social-auth/', include('social_django.urls', namespace="social")),
-  path('register/', RegisterView.as_view(), name='register'),
-  path('contact/', ContactUs.as_view(), name='contact'),
-  path('', base_views.home, name='home'),
-  path('refer', ReferView.as_view(), name='refer'),
-  path('about', base_views.about, name='about'),
-  path('contact', base_views.contact, name='contact'),
-  path('career', base_views.career, name='career'),
-  path('advertising', base_views.advertising, name='advertising'),
-  path('terms', base_views.terms, name='terms'),
-  path('privacy',  base_views.privacy, name='privacy'),
-  path('social', include('social_django.urls', namespace='social')),
-  path('psychometric', PsychoView.as_view(), name='psycho' ),
-  path('compare', CompareView.as_view(), name='compare')
+    path('admin/', admin.site.urls),
+    path('', base_views.home, name='home'),
+    path('college/', include(('college.urls', 'college'), namespace='college')),
+    path('profile/', include(('profiles.urls', 'profile'), namespace='profile')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('accounts/', include('allauth.urls')),
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    #   path('social-auth/', include('social_django.urls', namespace="social")),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('contact/', ContactUs.as_view(), name='contact'),
+    path('refer', ReferView.as_view(), name='refer'),
+    path('about', base_views.about, name='about'),
+    path('contact', base_views.contact, name='contact'),
+    path('career', base_views.career, name='career'),
+    path('advertising', base_views.advertising, name='advertising'),
+    path('terms', base_views.terms, name='terms'),
+    path('privacy',  base_views.privacy, name='privacy'),
+    path('social', include('social_django.urls', namespace='social')),
+    path('psychometric', PsychoView.as_view(), name='psycho'),
+    path('compare', CompareView.as_view(), name='compare'),
+    path('robots.txt', TemplateView.as_view(template_name="v2/seo/robots.txt", content_type="text/plain"), name='robots'),
+    path('sitemap.xml', TemplateView.as_view(template_name="v2/seo/sitemap.xml", content_type="text/plain"), name='sitemap')
 ]
 
 if settings.DEBUG:
