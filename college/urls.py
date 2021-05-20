0acc7f80f01b451c7a54ea0dc6104441bc7cbce9
Chayan Datta <sonicxxx7@gmail.com>
2021-05-20 23:45:05 +0530

    plugged bookmark view

--- college/urls.py
+++ college/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from college.views.public import IndividualCollegeView, CollegesView
+from college.views.public import AddBookmarkView, IndividualCollegeView, CollegesView
 
 urlpatterns = [
     path('list/', CollegesView.as_view(), name='list'),
-    path('<slug>/', IndividualCollegeView.as_view(), name='individual')
+    path('<slug>/', IndividualCollegeView.as_view(), name='individual'),
+    path('bookmark/<slug>/', AddBookmarkView.as_view(), name='bookmark')
 ]
