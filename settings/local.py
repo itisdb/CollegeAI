14080db9e8020b6492d52613c84c49d86f250c74
Chayan Datta <sonicxxx7@gmail.com>
2021-05-27 20:43:44 +0530

    adds notebook support

--- settings/local.py
+++ settings/local.py
@@ -6,3 +6,14 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mycollegeai',
+#         'USER': 'XXX',
+#         'PASSWORD': 'XXXX',
+#         'HOST': 'https://IP/',
+#         'PORT': 5432
+#     }
+# }
