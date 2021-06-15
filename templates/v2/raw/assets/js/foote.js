3bf77479eb88ac3adb2fc76a94c898d829c39afb
prihit <53579437+prihit@users.noreply.github.com>
2021-06-15 23:40:50 +0530

    List pages added to footer

--- templates/v2/raw/assets/js/foote.js
+++ templates/v2/raw/assets/js/foote.js
@@ -16,6 +16,24 @@ function topcityFunction() {
     }
   }
 
+  function otherFunction() {
+    document.getElementById("other").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
   function topstateFunction() {
     document.getElementById("top_state").classList.toggle("show");
   }
