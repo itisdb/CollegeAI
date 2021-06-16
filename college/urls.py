33ca598e9446ee0927d92e707ca4c5499ae9b781
prihit <53579437+prihit@users.noreply.github.com>
2021-06-16 21:43:37 +0530

    Changes added

--- college/urls.py
+++ college/urls.py
@@ -6,7 +6,7 @@ from college.views.temporary import SEOCollegesView, topCollegesView
 
 urlpatterns = [
     path('best-engineering-colleges-in-<location>/', SEOCollegesView.as_view(), name='seo-list'),
-    path('list-of-all-<college>-Universities-in-India/', topCollegesView.as_view(), name='top-list'),
+    path('list-of-all-<college>-universities-in-India/', topCollegesView.as_view(), name='top-list'),
     path('list/', CollegesView.as_view(), name='list'),
     path('apply/<college_uuid>/', ApplyCollegeView.as_view(), name='apply'),
     path('<slug>/', IndividualCollegeView.as_view(), name='individual'),
