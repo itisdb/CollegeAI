b2c8e22a0372acefadf681f071d80cced8e01306
prihit <53579437+prihit@users.noreply.github.com>
2021-07-14 08:47:26 +0530

    url changes done

--- college/urls.py
+++ college/urls.py
@@ -6,9 +6,9 @@ from college.views.temporary import SEOCollegesView, topCollegesView, tagCollege
 
 urlpatterns = [
     path('best-colleges-in-<location>/', SEOCollegesView.as_view(), name='seo-list'),
+    path('list-of-all-<college>-colleges-in-India/', topCollegesView.as_view(), name='top-list'),
     path('best-<tag>-colleges-in-<location>/', taglocCollegesView.as_view(), name='tagloc-list'),
     path('<tag>-colleges-in-India/', tagCollegesView.as_view(), name='tag-list'),
-    path('list-of-all-<college>-colleges-in-India/', topCollegesView.as_view(), name='top-list'),
     path('list/', CollegesView.as_view(), name='list'),
     path('apply/<college_uuid>/', ApplyCollegeView.as_view(), name='apply'),
     path('<slug>/', IndividualCollegeView.as_view(), name='individual'),
